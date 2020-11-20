d6ce518996cffd338887a07c46bf1678d3e68467
Nitik Narang <nitiknarang77@gmail.com>
2020-11-21 00:25:00 +0530

    make it better

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app=express();
 const mongoose=require('mongoose');
 const PORT=process.env.PORT || 5000;
 const Database=require('./config/keys');
-const cors=require('cors');
+
 
 
 
@@ -20,7 +20,7 @@ mongoose.connection.on('error',(err)=>{
 
 require('./models/user');
 require('./models/post');
-app.use(cors());
+
 app.use(express.json());
 app.use(require('./routes/user'));
 app.use(require('./routes/auth'));
@@ -29,7 +29,7 @@ app.use(require('./routes/post'));
 if(process.env.NODE_ENV=="production"){
     app.use(express.static('try/build'))
     const path=require('path');
-    app.get("*",(req,res)=>{
+    app.get("/*",(req,res)=>{
         res.sendFile(path.resolve(__dirname,"try","build","index.html"))
     })
 }
